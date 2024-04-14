4b8d2390dbd3c3020833d0424f28a1f0e197b027
Yoac <cayocutrim1991@gmail.com>
2024-04-14 20:31:02 -0300

    adding langchain4j dependencies

---
+++ src/main/java/com/yoaceng/aiassistantweb/AiAssistantWebApplication.java
@@ -0,0 +1,13 @@
+package com.yoaceng.aiassistantweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAssistantWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAssistantWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoaceng/aiassistantweb/AiAssistantWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoaceng.aiassistantweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAssistantWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
